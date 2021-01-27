d9abb3039010dcbd18930d5dbb683e5989f27483
weedust <ldc4@vip.qq.com>
2021-01-27 14:53:45 +0800

    client:区分开发和生产环境调用接口的端口

--- client/src/api/config.js
+++ client/src/api/config.js
@@ -1,4 +1,6 @@
+const port = process.env.NODE_ENV === 'production' ? '3210' : '8080'
+
 export default {
-  baseURL: 'http://localhost:8080',
+  baseURL: `http://localhost:${port}`,
   timeout: 1000,
 }
